a331e27b5a81c8656d900cbe3eb1b0cf73839831
Hazama65 <hazamac93@gmail.com>
2023-05-25 21:56:26 -0600

    segunda subida

--- JS/login.js
+++ JS/login.js
@@ -1,16 +1,14 @@
 const boton =document.getElementById("btnLogin");
 const inputUsuario =document.getElementById("usuario");
 const inputPassword =document.getElementById("password");
+const inputCorreo =document.getElementById("correo");
+
 
 const validations=(event)=>{
     let mensaje="";
-    if(inputUsuario.value && inputUsuario.value.length >0){
-        if(inputPassword.value && inputPassword.value.length >0){
-            mensaje='Campos correctos';
-        }else{
-            mensaje='Faltan datos';
-        }
-    }else{
+    if(inputUsuario.value && inputUsuario.value.length >0 && inputPassword.value && inputPassword.value.length >0 && inputCorreo.value && inputCorreo.value.length >0){
+        mensaje='Campos correctos';}
+        else{
         mensaje='Campos incorrecto';
     }
     alert(mensaje);
